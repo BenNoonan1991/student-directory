85bdeeb543f4a44d1d079b113b55519d6936bb7b
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-29 12:44:58 +0100

    Ex6 - format with center

--- directory.rb
+++ directory.rb
@@ -19,18 +19,18 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50, '-')
+  puts "-------------".center(50)
 end
 
 def print(students)
 students.each_with_index do |student, index|
-  puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort, #{student[:nationality]})"
+  puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort, #{student[:nationality]})".center(50)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
