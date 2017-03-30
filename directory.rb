a24c00bd37a5d2cfe75099a994ec198ad029b6df
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-30 17:29:46 +0100

    Step 11 - Save the data to a file

--- directory.rb
+++ directory.rb
@@ -37,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students."
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # Because we'll be adding more variables.
 end
 
@@ -52,6 +53,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else puts "I don't know what you mean, try again"
@@ -82,6 +85,18 @@ def print_footer
     puts "Overall, we have #{@students.count} great students".center(50)
   end
 end
+end
+
+def save_students
+  #Open the file for writing
+  file = File.open("students.csv", "w")
+  #Iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
