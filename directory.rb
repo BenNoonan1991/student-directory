d43b83c985b25e85ec9d7c88254de902fca64d04
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-31 13:25:56 +0100

    14.3 - Refactor process method

--- directory.rb
+++ directory.rb
@@ -17,17 +17,13 @@ end
 
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else puts "I don't know what you mean, try again"
+  when "1" then input_students
+  when "2" then show_students
+  when "3" then save_students
+  when "4" then load_students
+  when "9" then exit
+  else
+    puts "I don't know what you mean, try again"
   end
 end
 
